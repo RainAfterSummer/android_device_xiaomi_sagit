45e98c565b55640e976b31e35fc776cc78253fdb
AlicizationHash <13465717387@163.com>
2020-11-14 17:10:11 +0800

    sagit: Apply to EvolutionX

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/revengeos_sagit.mk
+    $(LOCAL_DIR)/evolution_sagit.mk
 
 COMMON_LUNCH_CHOICES := \
-    revengeos_sagit-user \
-    revengeos_sagit-userdebug \
-    revengeos_sagit-eng
+    evolution_sagit-user \
+    evolution_sagit-userdebug \
+    evolution_sagit-eng
--- revengeos_sagit.mk
+++ revengeos_sagit.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/sagit/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/revengeos/config/common.mk)
+$(call inherit-product, vendor/revengeos/config/common_full_phone.mk)
 
 PRODUCT_NAME := revengeos_sagit
 PRODUCT_DEVICE := sagit
